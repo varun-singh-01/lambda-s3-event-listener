a00b2f2be8ca67ba2e65eccff6233fba870e2da4
Varun Singh <varun@atliq.com>
2021-07-06 07:17:38 +0530

    Initial Commit

---
+++ cdk_lambda/cdk_lambda_stack.py
@@ -0,0 +1,51 @@
+# @author Varun Singh
+# @email admin@talkhash.com
+# @create date 2021-06-19 15:45:54
+# @modify date 2021-06-19 15:45:54
+# @desc AWS CDK Lambda + S3 Event Stack
+
+
+from aws_cdk import (
+    aws_iam as iam,
+    aws_lambda as _lambda,
+    aws_s3 as s3,
+    aws_s3_notifications as s3_notifications,
+    core as cdk
+)
+
+
+class CdkLambdaStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # create new IAM group and use
+        group = iam.Group(self, "VSGroup")
+        user = iam.User(self, "VSUser")
+
+        # Add IAM user to the group
+        user.add_to_group(group)
+
+        # create S3 Bucket
+        bucket = s3.Bucket(self, 'vs-bucket')
+        bucket.grant_read_write(user)
+
+        # create a lambda function
+        lambda_func = _lambda.Function(
+            self, 'LambdaListener',
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler='LambdaListener.handler',
+            code=_lambda.Code.asset('cdk_lambda\lambda'),
+            environment={
+                'BUCKET_NAME': bucket.bucket_name
+            })
+
+        # create trigger for Lambda function with image type suffixes
+        notification = s3_notifications.LambdaDestination(lambda_func)
+        notification.bind(self, bucket)
+        bucket.add_object_created_notification(
+            notification, s3.NotificationKeyFilter(suffix='.jpg'))
+        bucket.add_object_created_notification(
+            notification, s3.NotificationKeyFilter(suffix='.jpeg'))
+        bucket.add_object_created_notification(
+            notification, s3.NotificationKeyFilter(suffix='.png'))
---
+++ templates/asset.ce31b798eeca65afa86aa8381392785d2baaf4b0a03492f1dc622f5e695d9e7c/LambdaEventWriter.py
@@ -0,0 +1,43 @@
+# @author Varun Singh
+# @email admin@talkhash.com
+# @create date 2021-06-19 15:45:54
+# @modify date 2021-06-19 15:45:54
+# @desc AWS CDK Lambda + S3 Event Stack
+
+
+import boto3
+import os
+import json
+
+s3 = boto3.client('s3')
+dynamodb = boto3.client('dynamodb')
+
+
+def handler(event, context):
+    bucket_name = (os.environ['BUCKET_NAME'])
+    key = event['Records'][0]['s3']['object']['key']
+    image = {
+        'S3Object': {
+            'Bucket': bucket_name,
+            'Name': key
+        }
+    }
+
+    try:
+        print("[handler] :: {}".format(key))
+
+        # Write results to DynamoDB
+        dynamodb.put_item(TableName=(os.environ['TABLE_NAME']),
+                          Item={
+            'image_name': {'S': key}
+        }
+        )
+
+        response = {'status': 'success', 'key': key}
+        return response
+
+    except Exception as e:
+        print(e)
+        print("[Error] :: Error processing object {} from bucket {}. ".format(
+            key, bucket_name))
+        raise e
